c8919d6e5de7a9665c3646fcdef3a2cb80cef11a
ahnanne <54733637+ahnanne@users.noreply.github.com>
2023-02-05 01:30:56 +0900

    feat: App.tsx로부터 WebRTC Demo 페이지 분리

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import App from 'App';
 import GlobalStyles from 'lib/GlobalStyles';
 import RecoilProvider from 'components/providers/RecoilProvider';
 import QueryProvider from 'components/providers/QueryProvider';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryProvider>
       <RecoilProvider>
         <GlobalStyles />
-        <App />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </RecoilProvider>
     </QueryProvider>
   </React.StrictMode>,
